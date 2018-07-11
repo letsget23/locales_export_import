20ae4b76dae8dba3918eba514f4311444f09b8c6
letsget23 <letsget23@gmail.com>
2018-07-11 16:34:48 +0900

    Fix nil class error

--- lib/locales_export_import/csv2yaml.rb
+++ lib/locales_export_import/csv2yaml.rb
@@ -39,7 +39,7 @@ module LocalesExportImport
         hash[key] = value if hash.is_a?(::Hash)
       else
         head, _, tail = key.partition('.')
-        hash[head] = ::Hash.new if !hash.has_key?(head) || nil
+        hash[head] = ::Hash.new if hash.nil? || !hash.has_key?(head)
         add_value_to_tree(hash[head], tail, value)
       end
     end
